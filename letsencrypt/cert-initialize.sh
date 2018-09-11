4a92c50939e8f4d4e2afd715bb93e805b6d3d4c9
Brad Jolicoeur <brad@jolicoeurs.net>
2018-09-10 21:42:32 -0400

    remove cert component and verify that site is being created properly

--- letsencrypt/cert-initialize.sh
+++ letsencrypt/cert-initialize.sh
@@ -2,19 +2,20 @@ sudo mkdir -p /srv/docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-s
 
 curl -o /srv/docker/letsencrypt-docker-nginx/src/letsencrypt/docker-compose.yml https://raw.githubusercontent.com/bradjolicoeur/bradjolicoeur/master/letsencrypt/docker-compose.yml
 
+sudo mkdir -p /srv/docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site/
 curl -o /srv/docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site/index.html https://raw.githubusercontent.com/bradjolicoeur/bradjolicoeur/master/letsencrypt/index.html
 
 cd /srv/docker/letsencrypt-docker-nginx/src/letsencrypt
 sudo docker-compose -f /srv/docker/letsencrypt-docker-nginx/src/letsencrypt/docker-compose.yml up -d
 
-sudo docker run -it --rm \
--v /srv/docker-volumes/etc/letsencrypt:/etc/letsencrypt \
--v /srv/docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
--v /srv/docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site:/data/letsencrypt \
--v "/srv/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" \
-certbot/certbot \
-certonly --webroot \
---register-unsafely-without-email --agree-tos \
---webroot-path=/data/letsencrypt \
---staging \
--d bradjolicoeur.com -d www.bradjolicoeur.com
+# sudo docker run -it --rm \
+# -v /srv/docker-volumes/etc/letsencrypt:/etc/letsencrypt \
+# -v /srv/docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
+# -v /srv/docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site:/data/letsencrypt \
+# -v "/srv/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" \
+# certbot/certbot \
+# certonly --webroot \
+# --register-unsafely-without-email --agree-tos \
+# --webroot-path=/data/letsencrypt \
+# --staging \
+# -d bradjolicoeur.com -d www.bradjolicoeur.com
