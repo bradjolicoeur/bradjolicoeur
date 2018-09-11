7757f9292c8fa9fd79eb900b797756773883a6fc
Brad Jolicoeur <brad@jolicoeurs.net>
2018-09-10 21:18:04 -0400

    adding cert initialization scripts

---
+++ letsencrypt/cert-initialize.sh
@@ -0,0 +1,20 @@
+sudo mkdir -p /srv/docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site
+
+curl -o /srv/docker/letsencrypt-docker-nginx/src/letsencrypt https://raw.githubusercontent.com/bradjolicoeur/bradjolicoeur/master/letsencrypt/docker-compose.yml
+
+curl -o /srv/docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site/index.html https://raw.githubusercontent.com/bradjolicoeur/bradjolicoeur/master/letsencrypt/index.html
+
+cd /srv/docker/letsencrypt-docker-nginx/src/letsencrypt
+sudo docker-compose up -d
+
+sudo docker run -it --rm \
+-v /srv/docker-volumes/etc/letsencrypt:/etc/letsencrypt \
+-v /srv/docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
+-v /srv/docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site:/data/letsencrypt \
+-v "/srv/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" \
+certbot/certbot \
+certonly --webroot \
+--register-unsafely-without-email --agree-tos \
+--webroot-path=/data/letsencrypt \
+--staging \
+-d bradjolicoeur.com -d www.bradjolicoeur.com
