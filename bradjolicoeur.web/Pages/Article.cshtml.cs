c1ba5ca088e1577a645af60bcaaead6c1815e959
Brad Jolicoeur <brad@jolicoeurs.net>
2018-09-19 16:10:25 -0400

    Add privacy policy and google analytics

--- bradjolicoeur.web/Pages/Article.cshtml.cs
+++ bradjolicoeur.web/Pages/Article.cshtml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using bradjolicoeur.Core.Models.ContentType;
 using bradjolicoeur.web.ViewModels;
