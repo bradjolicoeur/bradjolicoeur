3f2f1e004001610609fc2a8c235313e30c96b033
Brad Jolicoeur <brad@jolicoeurs.net>
2020-11-14 20:08:24 -0500

    pages are awesome

--- bradjolicoeur.web/Pages/Reading.cshtml.cs
+++ bradjolicoeur.web/Pages/Reading.cshtml.cs
@@ -11,6 +11,14 @@ namespace bradjolicoeur.web.Pages
 {
     public class ReadingModel : PageModel
     {
+        [FromQuery(Name = "currentpage")]
+        public int CurrentPage { get; set; } = 1;
+
+        [FromQuery(Name = "length")]
+        public int PageSize { get; set; } = 10;
+        public long Count { get; set; }
+        public int TotalPages => (int)Math.Ceiling(decimal.Divide(Count, PageSize));
+
         private readonly IContentsClient<ReadingFeed, ReadingFeedData> _readingFeed;
 
         public ReadingModel(IContentsClient<ReadingFeed, ReadingFeedData> readingFeed)
@@ -20,12 +28,21 @@ namespace bradjolicoeur.web.Pages
 
         public ContentsResult<ReadingFeed, ReadingFeedData> content;
 
+        public bool NextPage => (CurrentPage * PageSize) <= content.Total;
+
+        public bool PreviousPage => (CurrentPage > 1);
+
         public async Task OnGetAsync()
         {
+
             content = await _readingFeed.GetAsync(new ContentQuery
             {
-                OrderBy = $"data/dateposted/iv desc"
-            });
+                OrderBy = $"data/dateposted/iv desc",
+                Skip = ((CurrentPage - 1) * PageSize),
+                Top = PageSize,
+            }) ;
+
+            Count = content.Total;
         }
     }
 }
